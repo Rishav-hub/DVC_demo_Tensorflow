ae72dc57ef57e6f0bee41006ae123078e95d3e8a
Rishav-hub <vibhanshuchauhan200@gmail.com>
2021-10-10 09:29:34 +0530

    Updated Second stage

--- src/stage_02_prepare_base_model.py
+++ src/stage_02_prepare_base_model.py
@@ -1,11 +1,9 @@
 from src.utils.all_utils import read_yaml, create_directory
-from src.utils.models import get_VGG_16_model
+from src.utils.models import get_VGG_16_model, prepare_model
 import argparse
-import pandas as pd
 import os
-import shutil
-from tqdm import tqdm
 import logging
+import io
 
 logging_str = "[%(asctime)s: %(levelname)s: %(module)s]: %(message)s"
 log_dir = "logs"
@@ -29,28 +27,39 @@ def prepare_base_model(config_path, params_path):
 
     base_model_path = os.path.join(base_model_dir_path, base_model_name)
 
-    model = get_VGG_16_model(input_shape= params['IMAGE_SIZE'], model_path= base_model_path)
+    model = get_VGG_16_model(input_shape= params['IMAGE_SIZE'],
+     model_path= base_model_path)
 
+    model = prepare_model(model, classes= params['CLASSES'],
+     freeze_all= True, freeze_layers= None,
+    learning_rate= params['LEARNING_RATE'])
 
+    updated_base_model_path = os.path.join(base_model_dir_path,
+     artifacts['UPDATED_BASE_MODEL_NAME'])
 
+    def _log_model_summary(full_model):
+        with io.StringIO() as stream:
+            full_model.summary(print_fn=lambda x: stream.write(f"{x}\n"))
+            summary_str = stream.getvalue()
+        return summary_str
 
+    logging.info(f"full model summary: \n{_log_model_summary(model)}")
 
-
-
+    model.save(updated_base_model_path)
 
 
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
 
     args.add_argument("--config", "-c", default="config/config.yaml")
-    args.add_argument("--params", "-p", default="config.yaml")
+    args.add_argument("--params", "-p", default="params.yaml")
 
     parsed_args = args.parse_args()
 
     try:
-        logging.info(">>>>> stage one started")
-        get_data(config_path=parsed_args.config)
-        logging.info("stage one completed! all the data are saved in local >>>>>")
+        logging.info(">>>>> stage two started")
+        prepare_base_model(config_path=parsed_args.config, params_path=parsed_args.params)
+        logging.info("stage two completed! base model is created >>>>>")
     except Exception as e:
         logging.exception(e)
         raise e
--- src/utils/models.py
+++ src/utils/models.py
@@ -13,5 +13,33 @@ def get_VGG_16_model(input_shape, model_path):
     Returns a VGG16 model with the weights pre-trained on ImageNet.
     """
     model = VGG16(weights='imagenet', include_top=False, input_shape=input_shape)
-    model.load_weights(model_path)
+    model.save(model_path)
+    logging.info(f"VGG16 model loaded saved at {model_path}")
     return model
+
+def prepare_model(model, classes,freeze_all, freeze_layers, learning_rate):
+    """
+    Compiles and returns a model.
+    """
+    if freeze_all:
+        for layer in model.layers:
+            layer.trainable = False
+    else:
+        for layer in model.layers[:freeze_layers]:
+            layer.trainable = False
+        for layer in model.layers[freeze_layers:]:
+            layer.trainable = True
+    
+    flatten_layer = tf.keras.layers.Flatten()
+
+    predictions = tf.keras.layers.Dense(classes, activation='softmax')(flatten_layer(model.output))
+    full_model = tf.keras.Model(inputs=model.input, outputs=predictions)
+
+
+    full_model.compile(optimizer=tf.keras.optimizers.SGD(learning_rate= learning_rate),
+                  loss='categorical_crossentropy',
+                  metrics=['accuracy'])
+
+    logging.info("Custom model is compiled and ready to be trained")
+
+    return full_model
